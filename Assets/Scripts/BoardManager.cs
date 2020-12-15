7c3f69fbd976bd5b633fe9c60c0337e3d5ed3535
MikelArinaMarcos <mikel.arina@estudiantat.upc.edu>
2020-12-15 14:01:30 +0100

    Creación inicial niveles

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+	public void SetUpScene()
+    {
+        Debug.Log("Ejecutado");
+    }
+}
