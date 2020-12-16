2a5f35d0ae3e785165dfe51f396ed84f5adcc748
MikelArinaMarcos <mikel.arina@estudiantat.upc.edu>
2020-12-16 19:29:44 +0100

    Creación del suelo y los muros

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour {
 
-	public void SetUpScene()
+    public int columns = 8;
+    public int rows = 8;
+
+    public GameObject[] floorTiles, outerWallTiles; // Losetas
+
+
+    public void SetUpScene()
     {
-        Debug.Log("Ejecutado");
+        BoardSetUp();
+    }
+
+    void BoardSetUp()
+    {
+        for (int x = -1; x < columns + 1; x++)
+        {
+            for (int y = -1; y < rows + 1; y++)
+            {
+                GameObject toInstantiate = GetRandomInArray(floorTiles);
+
+                if (x == -1 || y == -1 || x == columns || y == rows)
+                {
+                    toInstantiate = GetRandomInArray(outerWallTiles);
+                }
+
+                Instantiate(toInstantiate, new Vector2(x, y), Quaternion.identity);
+            }
+        }
+    }
+
+    GameObject GetRandomInArray(GameObject[] array)
+    {
+        return array[Random.Range(0, array.Length)];
     }
 }
