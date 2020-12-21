8cfb62defd1a8cefce530fae8bfd4eeb7dd4b99b
MikelArinaMarcos <mikel.arina@estudiantat.upc.edu>
2020-12-21 13:13:37 +0100

    Programación rivales

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MovingObject
+{
+    public int playerDamage;
+
+    private Animator animator;
+    private Transform target;
+    private bool skipMove;
+
+    protected override void Awake()
+    {
+        animator = GetComponent<Animator>();
+        base.Awake();
+    }
+
+    protected override void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        base.Start();
+    }
+
+
+
+    protected override void OnCantMove(GameObject go)
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
